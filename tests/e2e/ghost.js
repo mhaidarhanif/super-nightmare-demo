9d1c1409a0605c6ca35ac0d1e047377a1337b9f8
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-11 18:22:53 +0700

    Test Ghost stable

--- tests/e2e/ghost.js
+++ tests/e2e/ghost.js
@@ -4,15 +4,16 @@ const expect = chai.expect
 const should = chai.should()
 
 const nightmare = Nightmare({
-	// waitTimeout: 5000,
-	// gotoTimeout: 5000,
-	// loadTimeout: 5000,
+	// openDevTools: {
+	// 	mode: 'detach'
+	// },
+	waitTimeout: 50000,
+	gotoTimeout: 50000,
+	loadTimeout: 50000,
 	width: 1000,
 	height: 600,
-	show: true,
-	openDevTools: {
-		mode: 'detach'
-	}
+	typeInterval: 20,
+	show: true
 })
 
 const URL = 'http://localhost:2368/ghost'
@@ -50,7 +51,9 @@ describe('Create new post', function () {
 			.wait(1000) // hold on
 			.click('li.post-save-publish a') // publish draft
 			.click('li.delete a') // delete post
+			.wait(1000)
 			.click('button.btn-red') // confirm delete
+			.wait(1000)
 			.then(function () {
 				done()
 			})
@@ -59,23 +62,7 @@ describe('Create new post', function () {
 			})
 	})
 
-	it.skip('Expect to have the post published', function (done) {
-		nightmare
-			.goto(URL)
-			.click('a.post-edit') // edit post
-			.pause(3000)
-			.type('input#entry-title', 'Hello Universe!')
-			.type('input.markdown-editor', 'Hello people of the universe!')
-			.click('button.js-publish-button') // save update
-			.evaluate(function () {
-				return document.querySelector('#main .searchCenterMiddle li a').href
-			})
-			.end()
-			.then(function (link) {
-				expect(link).to.equal('https://github.com/segmentio/nightmare')
-				done()
-			})
-	})
+	it.skip('Expect to have the post published', function (done) {})
 })
 
 describe('Edit post', function () {
@@ -88,8 +75,7 @@ describe('Edit post', function () {
 			.click('.post-controls a.post-edit')
 			.type('input#entry-title', 'Hello Universe!')
 			.type('textarea.markdown-editor', 'Hello people of this universe!')
-			.click('li.post-save-draft a') // save draft first
-			.wait(1000) // hold on
+			.wait(5000) // wait for auto save
 			.then(function () {
 				done()
 			})
@@ -99,7 +85,7 @@ describe('Edit post', function () {
 	})
 })
 
-describe.only('Delete post', function () {
+describe('Delete post', function () {
 	this.timeout(15000)
 
 	it('Expect to be able to delete latest post', function (done) {
@@ -110,6 +96,7 @@ describe.only('Delete post', function () {
 			.wait('textarea.markdown-editor')
 			.click('li.delete a') // delete post
 			.click('button.btn-red') // confirm delete
+			.wait(1000)
 			.then(function () {
 				done()
 			})
